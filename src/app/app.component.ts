12cd3c239d17ddc7716994338eea23dbb9abebac
Carlos <Nitro@localhost.localdomain>
2024-02-01 20:05:57 +0100

    añado JSON y lo imprimo en la página

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import * as preguntas_harry_potter from 'src/assets/preguntas_harry_potter.json';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'HogwartsPedromi';
+
+  cuestionario: any = preguntas_harry_potter;
+  mapa_puntuaciones: Record<string, number> = {/* 
+    'Ravenclaw': 0,
+    'Slytherin': 0,
+    'Hufflepuff': 0,
+    'Gryffindor': 0, */
+  };
+
+  variable_temporal_para_imprimir: any = this.imprimir_cuestionario();
+
+  imprimir_cuestionario() {
+    console.log(this.cuestionario);
+  }
+
+  elegirRespuesta(casa: string, puntos: number) {
+    if (this.mapa_puntuaciones[casa] == undefined) {
+      this.mapa_puntuaciones[casa] = puntos;
+    } else {
+      this.mapa_puntuaciones[casa] += puntos;
+    }
+
+    console.log(this.mapa_puntuaciones);
+  }
 }
